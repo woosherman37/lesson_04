e2521d3ab91fb0aeb28f1ecdee1db4de99a65fb3
thomas0376 <thomas.prevarin@gmail.com>
2024-11-13 17:19:58 +0000

    Replaced If statement with Dictionary

--- main.py
+++ main.py
@@ -6,12 +6,11 @@ order = input(f"We offer Latte, Americano or Cappuccino? ")
 
 print(f"\n{order}\n")
 
-if order == "Latte": 
-	price = 4
-elif order == "Americano":
-	price = 3
-else:
-	price = 3.5
+
+price_list = {"latte":4, "americano":3, "cappuccino":3.5}
+
+price = price_list[order]
+
 
 print(f"It's £{price} in total.")
 
